682e1fcf6ce4e31ddde2361d138afc4eb93efafa
Caio-eng <59921705+Caio-eng@users.noreply.github.com>
2021-01-24 17:30:11 -0300

    Initial commit

---
+++ src/main/java/domain/Produto.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+public class Produto {
+	
+	@Id
+	private Integer id;
+	
+	@Column(length = 50, nullable = false, unique = true)
+	private String nome;
+	
+	@Column(nullable = false)
+	private Integer quantidade;
+	
+	@Column(nullable = false, precision = 5, scale = 2)
+	private BigDecimal preco;
+	
+	@Column
+	private LocalDate dataDeValidade;
+}
